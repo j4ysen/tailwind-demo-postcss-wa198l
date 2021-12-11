894050600f573d5c021580c5e0ed9dd542860207
j4ysen <j4ysen@users.noreply.github.com>
2021-12-11 15:33:52 +0000

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -27,4 +27,43 @@ module.exports = {
     },
 
   },
+  darkMode: false,
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000000',
+      white: '#FFFFFF',
+      pageBG: 'var(--pageBG)',
+      pageText: 'var(--pageText)',
+    },
+    screens: {
+      xs: '480px',
+      sm: '768px',
+      md: '940px',
+      lg: '1200px',
+      xl: '1600px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Nimbus Sans L', 'system-ui', "Segoe UI", "Helvetica Neue", 'Arial', "Noto Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        serif: ['Argesta Hairline', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+        inherit: 'inherit',
+      },
+      fontSize: {
+        xxs: '.625rem',
+      },
+      zIndex: {
+        '-1': '-10',
+        50: 50,
+        60: 60,
+        70: 70,
+        80: 80,
+        90: 90,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
 }
